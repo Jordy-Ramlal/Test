1e14acfad9102c7e12111933e44d0a3166707389
Jordy-Ramlal <19057555@student.hhs.nl>
2020-11-16 10:42:48 +0100

    Clone

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -131,7 +131,7 @@ namespace myfirstapp
             {
                 fall();
                 drawNewPosition();
-                deleteOldPosition();
+                
             }
             else
             {
